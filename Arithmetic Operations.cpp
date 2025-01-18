f1f7719162921fcfa874a5fdb2e562b1eea12ef9
anuraglondhe <anuraglondhe9052@gmail.com>
2025-01-18 15:57:19 +0530

    Create Arithmetic Operations.cpp

---
+++ Arithmetic Operations.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter a:";
+	cin>>a;
+	cout<<"Enter b:";
+	cin>>b;
+	
+	cout<<"Addition:"<<a+b<<endl;
+	cout<<"Subtraction:"<<a-b<<endl;
+	cout<<"Multiplication:"<<a*b<<endl;
+	cout<<"Division:"<<a/b<<endl;
+	cout<<"Mod:"<<a%b<<endl;
+	
+	return 0;
+}
